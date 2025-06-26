4d339568afd13a69000f52e14d7a587cbd22a0c8
Kostiantyn Teltov <dneprokos@gmail.com>
2025-06-26 20:30:29 +0200

    Updated sorting presentation

--- algorithms/finished/sorting/bubble_finished.py
+++ algorithms/finished/sorting/bubble_finished.py
@@ -3,17 +3,7 @@
 
 
 def bubbleSort(dataset):
-    # start with the array length and decrement each time
-    for i in range(len(dataset)-1, 0, -1):
-        # examine each item pair
-        for j in range(i):
-            # swap items if needed
-            if dataset[j] > dataset[j+1]:
-                temp = dataset[j]
-                dataset[j] = dataset[j+1]
-                dataset[j+1] = temp
-
-        print("Current state: ", dataset)
+    print("Current state: ", dataset)
 
 
 list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
